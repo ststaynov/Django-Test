d4bb8003952e26a81f9fbfd314da4521a0c49e50
ststaynov <ststaynov@hotmail.com>
2015-11-28 19:45:00 +0100

    Skigrade Project added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn skigrade.wsgi --log-file -
---
+++ grades/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Grade, Resort
+
+
+admin.site.register(Resort)
+admin.site.register(Grade)
