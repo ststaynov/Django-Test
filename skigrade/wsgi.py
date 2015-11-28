7bb4f558b1ba57909b69c849dcbc85bb970c4059
ststaynov <ststaynov@hotmail.com>
2015-11-28 19:54:30 +0100

    Deployment locally configured -  WORKING

--- skigrade/wsgi.py
+++ skigrade/wsgi.py
@@ -14,7 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skigrade.settings")
 
 application = get_wsgi_application()
-
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
