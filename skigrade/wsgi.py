70c1397d84ddfcffdc3b544812353202e8a5b230
ststaynov <ststaynov@hotmail.com>
2015-11-29 21:22:19 +0100

    optimized for web

--- skigrade/wsgi.py
+++ skigrade/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skigrade.settings")
 
 application = get_wsgi_application()
+#HEROKU
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
