2622b3956ccebe26bc8a3b90bb2d57050ca727b2
ststaynov <ststaynov@hotmail.com>
2016-02-14 17:47:46 +0100

    Djago with Gulp

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn skigrade.wsgi --log-file -
--- skigrade/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^grades/', include('grades.urls', namespace="grades")),
-    url(r'^admin/', include(admin.site.urls)),
-]
