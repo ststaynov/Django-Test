28175bdc5b7ffedbba7f4dc5a09f5f1872389a7b
ststaynov <ststaynov@hotmail.com>
2016-04-02 22:40:14 +0200

    close button ^ overlay ^ side-pattern = added

--- skiGrade/resorts/static/resorts/js/components/main.js
+++ skiGrade/resorts/static/resorts/js/components/main.js
@@ -6,3 +6,20 @@
 $('.c-grades').on('click', function () {
     $(this).css('background-color','red');
 });
+
+// open menu
+$('.e-menu-title').on('click', function () {
+    if($(this).hasClass('is-active')) {
+        $(this).removeClass('is-active');
+        $('.c-menu-overlay').removeClass('is-active');
+    } else{
+        $(this).addClass('is-active');
+        $('.c-menu-overlay').addClass('is-active');
+    }
+});
+
+// close menu when overlay is clicked
+$('.c-menu-overlay').on('click', function () {
+    $(this).removeClass('is-active');
+    $('.e-menu-title').removeClass('is-active');
+});
