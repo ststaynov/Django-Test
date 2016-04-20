a4fa7d449e52976f348add61cdbe00776955650a
ststaynov <ststaynov@hotmail.com>
2016-04-20 23:36:18 +0200

    Facebook correction database setup test

--- skiGrade/settings.py
+++ skiGrade/settings.py
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'skiGrade.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
+}
 # Heroku Start
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES = {}
 DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
