672b793e990b6d1badf3aab6d882b515b0888ea5
ststaynov <ststaynov@hotmail.com>
2016-05-03 21:36:38 +0200

    isauthenticated() fixed and working for homepage

--- resorts/views.py
+++ resorts/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import render_to_response
 
 
 def index(request):
-    return render_to_response('resorts/index.html', {})
-
-
-def login(request):
-    return render_to_response('resorts/login.html', {})
+    return render(request, 'resorts/index.html', {})
